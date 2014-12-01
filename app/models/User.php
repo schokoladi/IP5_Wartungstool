089b1b557e80d7fa827eaa064af635861c9530cb
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2014-12-01 13:44:19 +0100

    more user functionality and login form

--- app/models/User.php
+++ app/models/User.php
@@ -11,18 +11,28 @@ class User extends Eloquent {
     // Timestamps werden standardmässig von Laravel verlangt, überschreiben
     public $timestamps = false;
 
-	//use UserTrait, RemindableTrait;
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-
     // default-Werte ändern
 	protected $table = 'Users';
     protected $primaryKey = 'ID';
     protected $fillable = ['Name', 'Passwort', 'Email'];
+
+    public static $rules = [
+        'name' => 'required',
+        'password' => 'required'
+    ];
+
+    public $errors;
+
+    public function isValid(){
+
+        $validation = Validator::make($this->attributes, static::$rules);
+
+        if($validation->passes()) return true;
+
+        $this->errors = $validation->messages();
+
+        return false;
+    }
 
 	/**
 	 * The attributes excluded from the model's JSON form.
--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -3,15 +3,22 @@
 @section('content')
     <h1>Create new User</h1>
 
-    {{ Form::open() }}
+    {{ Form::open(['route' => 'users.store']) }}
         <div>
             {{ Form::label('Name', 'Name des Users: ') }}
             {{ Form::text('name') }}
+            {{ $errors->first('name') }}
+        </div>
+
+        <div>
+            {{ Form::label('Email', 'E-Mail: ') }}
+            {{ Form::text('e-mail') }}
         </div>
 
         <div>
             {{ Form::label('Passwort', 'Passwort: ') }}
-            {{ Form::password('Passwort') }}
+            {{ Form::password('password') }}
+            {{ $errors->first('password') }}
         </div>
         {{ Form::submit('Mach mau User') }}
     {{ Form::close() }}
