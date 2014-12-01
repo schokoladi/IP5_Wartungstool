2e70d00bfd31d5da8fc66e04ee68d519826c53e9
JanSchnider <jan.schnider@gmx.ch>
2014-12-01 15:12:35 +0100

    added createtest

--- app/routes.php
+++ app/routes.php
@@ -16,6 +16,7 @@ Route::get('/', 'PagesController@home');
 
 // so wird auf der /about-Seite PagesController.php mit der fkt about aufgerufen
 Route::get('/about', 'PagesController@about');
+Route::get('/create', 'PagesController@create');
 
 // Blade Test ('seite, welche aufgerufen wird') localhost/about
 //Route::get('users', 'UsersController@index');
