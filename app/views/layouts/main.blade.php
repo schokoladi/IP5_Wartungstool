e009e308e4ba55d972084813384135dc0a282468
JanSchnider <jan.schnider@gmx.ch>
2014-12-05 14:05:27 +0100

    design wip

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -5,21 +5,15 @@
 {{ HTML::style('css/main.css') }}
 </head>
 <body>
-    <div id="wrapper">
-        <div id="headerwrap">
-        <div id="header">
-            <p></p>
-        </div>
-        </div>
-        <div id="navigationwrap">
-        <div id="navigation">
-            menue {{Breadcrumbs::addCrumb('Home', '/')}}
-        </div>
-        </div>
-        <div id="contentwrap">
+    <div id="headerwrap">
+        <div id="header"></div>
+    </div>
+    <div id="navigationwrap">
+        <div id="navigation">menue {{Breadcrumbs::addCrumb('Home', '/')}}</div>
+    </div>
+    <div id="contentwrap">
         <div id="content">
             @yield('content')
-         </div>
         </div>
     </div>
 </body>
