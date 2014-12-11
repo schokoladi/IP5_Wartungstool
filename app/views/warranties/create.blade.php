f8b798bdda6f0a939e2f3747acf49d1911ed564d
JanSchnider <jan.schnider@gmx.ch>
2014-12-11 10:10:47 +0100

    design warranties

--- app/views/warranties/create.blade.php
+++ app/views/warranties/create.blade.php
@@ -20,34 +20,26 @@
         <tr>
             <td class = "col1">{{ Form::label('Artikel_ID', 'Artikel:') }}</td>
             <td class = "col2">{{ Form::select('Artikel_ID', $artikel_options , Input::old('Artikel_ID')) }}</td>
-            <td class = "col3"></td>
-            <td class = "col4">{{ Form::label('Name', 'Name: ') }} {{ $errors->first('Name') }}</td>
-            <td class = "col5">{{ Form::text('Name') }}</td>
-            <td class = "col6"></td>
+            <td class = "col3">{{ Form::label('Name', 'Name: ') }} {{ $errors->first('Name') }}</td>
+            <td class = "col4">{{ Form::text('Name', null, ['size'=>'30']) }}</td>
         </tr>
         <tr>
              <td class = "col1">{{ Form::label('Beschreibung', 'Beschreibung: ') }}</td>
              <td class = "col2">{{ Form::textarea('Beschreibung') }}</td>
              <td class = "col3"></td>
              <td class = "col4"></td>
-             <td class = "col5"></td>
-             <td class = "col6"></td>
         </tr>
         <tr>
-             <td class = "col1">{{ Form::label('Einkaufspreis', 'Einkaufspreis (CHF):') }} {{ $errors->first('Einkaufspreis') }}</td>
-             <td class = "col2">{{ Form::text('Einkaufspreis') }}</td>
-             <td class = "col3"></td>
-             <td class = "col4">{{ Form::label('Verkaufspreis', 'Verkaufspreis (CHF): ') }} {{ $errors->first('Verkaufspreis') }}</td>
-             <td class = "col5">{{ Form::text('Verkaufspreis') }}</td>
-             <td class = "col6"></td>
+             <td class = "col1">{{ Form::label('Einkaufspreis', 'Einkaufspreis:') }} {{ $errors->first('Einkaufspreis') }}</td>
+             <td class = "col2">{{ Form::text('Einkaufspreis', null, ['size'=>'8'])}} CHF</td>
+             <td class = "col3">{{ Form::label('Verkaufspreis', 'Verkaufspreis: ') }} {{ $errors->first('Verkaufspreis') }}</td>
+             <td class = "col4">{{ Form::text('Verkaufspreis', null, ['size'=>'8']) }} CHF</td>
         </tr>
         <tr>
-             <td class = "col1">{{ Form::label('Dauer', 'Dauer (Monate):') }} {{ $errors->first('Dauer') }}</td>
-             <td class = "col2">{{ Form::text('Dauer') }}</td>
+             <td class = "col1">{{ Form::label('Dauer', 'Dauer:') }} {{ $errors->first('Dauer') }}</td>
+             <td class = "col2">{{ Form::text('Dauer', null, ['size'=>'8']) }} Monate</td>
              <td class = "col3"></td>
              <td class = "col4"></td>
-             <td class = "col5"></td>
-             <td class = "col6"></td>
         </tr>
         </table>
 
