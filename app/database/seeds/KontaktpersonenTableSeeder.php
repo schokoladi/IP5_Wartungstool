d439992c69ea1c46b74fbc44f9a27577aec783d6
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2014-12-14 15:24:08 +0100

    added data and manual Kunden_ID to KontaktpersonenTableSeeder

--- app/database/seeds/KontaktpersonenTableSeeder.php
+++ app/database/seeds/KontaktpersonenTableSeeder.php
@@ -6,17 +6,53 @@ class KontaktpersonenTableSeeder extends Seeder {
     {
         // Werte löschen
         DB::table('Kontaktpersonen')->delete();
-        DB::table('Kontaktpersonen')->insert(
+        DB::table('Kontaktpersonen')->insert([
             [
                 'Name' => 'Jossen',
                 'Vorname' => 'Daniel',
                 'Rolle' => 'CEO',
                 'Typ' => 'Mitarbeiter',
-                'Kunden_ID' => 1,
+                'Kunden_ID' => 5,
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+            ],
+            [
+                'Name' => 'Jenny',
+                'Vorname' => 'Manuel',
+                'Rolle' => 'System Engineer',
+                'Typ' => 'Mitarbeiter',
+                'Kunden_ID' => 5,
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+            ],
+            [
+                'Name' => 'Gysel',
+                'Vorname' => 'Peter',
+                'Rolle' => 'Dozent',
+                'Typ' => 'Network',
+                'Kunden_ID' => 7,
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+            ],
+            [
+                'Name' => 'Page',
+                'Vorname' => 'Larry',
+                'Rolle' => 'Founder',
+                'Typ' => 'typ',
+                'Kunden_ID' => 6,
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+            ],
+            [
+                'Name' => 'Brin',
+                'Vorname' => 'Sergey',
+                'Rolle' => 'Founder',
+                'Typ' => 'typ',
+                'Kunden_ID' => 6,
                 'created_at' => new DateTime,
                 'updated_at' => new DateTime
             ]
-        );
+        ]);
     }
 
 }
