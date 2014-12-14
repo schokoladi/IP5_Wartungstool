7a63209fcc9e59025db4fd8b5bf11b3ca85d3f04
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2014-12-14 14:24:29 +0100

    another test (MassAssignmentException)

--- app/database/seeds/KundenTableSeeder.php
+++ app/database/seeds/KundenTableSeeder.php
@@ -7,14 +7,7 @@ class KundenTableSeeder extends Seeder {
         DB::table('Kunden')->delete();
         $customer = new Customer;
         $customer->fill([
-            'ID'       => 0,
             'Name'     => '--- select ---',
-            'Strasse'  => 'foo',
-            'Nr'       => 'bar',
-            'PLZ'      => 'foo',
-            'Ort'      => 'bar',
-            'E-Mail'   => 'foo',
-            'Telefon'  => 'bar',
             'order'	   => 0,
         ]);
         $customer->save();
