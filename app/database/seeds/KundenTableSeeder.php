24141151a6bed33f81c88426922ef387bd1aebdb
Dominik Schoch <dominik.schoch@students.fhnw.ch>
2014-12-14 14:31:39 +0100

    another try

--- app/database/seeds/KundenTableSeeder.php
+++ app/database/seeds/KundenTableSeeder.php
@@ -5,12 +5,18 @@ class KundenTableSeeder extends Seeder {
     public function run()
     {
         DB::table('Kunden')->delete();
-        $customer = new Customer;
-        $customer->fill([
-            'Name'     => '--- select ---',
-            'order'	   => 0,
+        DB::table('Kunden')->insert([
+            'Name' => '--- select ---',
+            'Strasse' => '--- select ---',
+            'Nr' => '--- select ---',
+            'PLZ' => '--- select ---',
+            'Ort' => '--- select ---',
+            'E-Mail' => '--- select ---',
+            'Telefon' => '--- select ---',
+            'order' => 0,
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime
         ]);
-        $customer->save();
     }
 
 }
